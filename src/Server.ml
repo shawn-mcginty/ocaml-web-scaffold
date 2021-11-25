4ee688dfb0061c56495ccec94f6acef473bacd2e
Shawn <mcginty.shawn@gmail.com>
2021-11-24 23:49:17 -0600

    feat: Enable cors by default

--- src/Server.ml
+++ src/Server.ml
@@ -1,3 +1,32 @@
+let enable_cors next req =
+  let headers =
+    [
+      ("Access-Control-Allow-Origin", "*");
+      ("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+      ( "Access-Control-Allow-Headers",
+        "Host, Connection, Pragma, Cache-Control, User-Agent, Accept, Sec-GPC, \
+         Origin, Sec-Fetch-Site, Sec-Fetch-Mode, Sec-Fetch-Dest, Referer, \
+         Accept-Language, Content-Type, Content-Length, Accept-Encoding, \
+         X-CSRF-Token, Authorization" );
+    ]
+  in
+  match Dream.method_ req with
+  | `OPTIONS -> Dream.respond ~status:`No_Content ~headers ""
+  | _ ->
+      let%lwt res = next req in
+      let res_w_headers =
+        res
+        |> Dream.add_header "Access-Control-Allow-Origin" "*"
+        |> Dream.add_header "Access-Control-Allow-Methods"
+             "POST, GET, OPTIONS, PUT, DELETE"
+        |> Dream.add_header "Access-Control-Allow-Headers"
+             "Host, Connection, Pragma, Cache-Control, User-Agent, Accept, \
+              Sec-GPC, Origin, Sec-Fetch-Site, Sec-Fetch-Mode, Sec-Fetch-Dest, \
+              Referer, Accept-Language, Content-Type, Content-Length, \
+              Accept-Encoding, X-CSRF-Token, Authorization"
+      in
+      Lwt.return res_w_headers
+
 let serve () =
   let default_db_pool_size = 10 in
   let db_pool_size =
@@ -12,8 +41,9 @@ let serve () =
     | Some p -> int_of_string p
   in
   let () = Printf.sprintf "listening on port %i" port |> print_endline in
-  Dream.serve @@ Dream.logger @@ Dream.origin_referer_check
+  Dream.serve @@ Dream.logger
   @@ Dream.sql_pool ~size:db_pool_size (Db_config.connection_string ())
+  @@ enable_cors
   @@ Dream.router
        [
          Dream.any "/api/v1/graphql" (Dream.graphql Lwt.return Schemas.all);
